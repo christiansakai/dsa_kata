e2bfae77e5f7c568d619da58ad1294b05def1361
Christian Sakai <christianmsakai@gmail.com>
2019-10-31 18:05:42 -0400

    Add triplets with smaller sum

--- patterns/two_pointers/triplets-with-smaller-sum.js
+++ patterns/two_pointers/triplets-with-smaller-sum.js
@@ -6,8 +6,20 @@
  */
 
 /**
+ * Sort the array.
+ * Iterate through the array. Every element in the iteration is
+ * our first number. To find the second number, we start from the
+ * first number index + 1, and the third number from the end of the array,
+ * If we find a sum that is less than target sum, we have found
+ * such triplet. Furthermore, since the array is sorted, now
+ * we know that valid tiplets also can be made by the
+ * numbers from the second number to the third number. We add
+ * this into the calculation.
+ *
+ * Time Complexity: O(N^2)
+ * Space Complexity: O(1)
 */
-function tripletsWithSmallerSum (arr, target) {
+function tripletsWithSmallerSum1 (arr, target) {
   if (!Array.isArray(arr)) {
     throw new Error('arr must be an Array')
   }
@@ -19,4 +31,90 @@ function tripletsWithSmallerSum (arr, target) {
   if (arr.length < 3) {
     return null
   }
+
+  const sortedArr = arr.sort((a, b) => a - b)
+  let tripletsCount = 0
+
+  for (let i = 0; i < sortedArr.length - 2; i++) {
+    let j = i + 1
+    let k = sortedArr.length - 1
+
+    while (j < k) {
+      const sum = sortedArr[i] + sortedArr[j] + sortedArr[k]
+
+      if (sum < target) {
+        tripletsCount += k - j
+
+        j += 1
+      } else {
+        k -= 1
+      }
+    }
+  }
+
+  return tripletsCount
+}
+
+/**
+ * Although a simpler approach with the same Time Complexity
+ * can be done by just iterating three nested loops. In this approach
+ * below we see how we can extend the code above.
+ * Sort the array.
+ * Iterate through the array. Every element in the iteration is
+ * our first number. To find the second number, we start from the
+ * first number index + 1, and the third number from the end of the array,
+ * If we find a sum that is less than target sum, we have found
+ * such triplet. Furthermore, since the array is sorted, now
+ * we know that valid tiplets also can be made by the
+ * numbers from the second number to the third number. We add
+ * this into the calculation.
+ *
+ * Time Complexity: O(N^2)
+ * Space Complexity: O(1)
+*/
+function tripletsWithSmallerSum2 (arr, target) {
+  if (!Array.isArray(arr)) {
+    throw new Error('arr must be an Array')
+  }
+
+  if (typeof target !== 'number') {
+    throw new Error('targetSum must be a number')
+  }
+
+  if (arr.length < 3) {
+    return null
+  }
+
+  const sortedArr = arr.sort((a, b) => a - b)
+  const triplets = []
+
+  for (let i = 0; i < sortedArr.length - 2; i++) {
+    let j = i + 1
+    let k = sortedArr.length - 1
+
+    while (j < k) {
+      const sum = sortedArr[i] + sortedArr[j] + sortedArr[k]
+
+      if (sum < target) {
+        for (let x = k; x > j; x--) {
+          triplets.push([
+            sortedArr[i],
+            sortedArr[j],
+            sortedArr[x]
+          ])
+        }
+
+        j += 1
+      } else {
+        k -= 1
+      }
+    }
+  }
+
+  return triplets
+}
+
+module.exports = {
+  tripletsWithSmallerSum1,
+  tripletsWithSmallerSum2
 }
--- patterns/two_pointers/triplets-with-smaller-sum.spec.js
+++ patterns/two_pointers/triplets-with-smaller-sum.spec.js
@@ -1,9 +1,9 @@
 const solution = require('./triplets-with-smaller-sum')
 
 describe('Triplets with Smaller Sum (medium)', () => {
-  describe('tripletsWithSmallerSum', () => {
-    it('search a triplet that is as close to target as possible', () => {
-      const result = solution.tripletsWithSmallerSum(
+  describe('tripletsWithSmallerSum1', () => {
+    it('count triplets which sum is smaller than target sum', () => {
+      const result = solution.tripletsWithSmallerSum1(
         [-1, 0, 2, 3],
         3
       )
@@ -11,13 +11,41 @@ describe('Triplets with Smaller Sum (medium)', () => {
       expect(result).toEqual(2)
     })
 
-    it('search a triplet that is as close to target as possible', () => {
-      const result = solution.tripletsWithSmallerSum(
+    it('count triplets which sum is smaller than targe sum', () => {
+      const result = solution.tripletsWithSmallerSum1(
         [-1, 4, 2, 1, 3],
         5
       )
 
       expect(result).toEqual(4)
+    })
+  })
+
+  describe('tripletsWithSmallerSum2', () => {
+    it('find all triplets which sum is smaller than target sum', () => {
+      const result = solution.tripletsWithSmallerSum2(
+        [-1, 0, 2, 3],
+        3
+      )
+
+      expect(result).toEqual([
+        [-1, 0, 3],
+        [-1, 0, 2]
+      ])
+    })
+
+    it('find all triplets which sum is smaller than targe sum', () => {
+      const result = solution.tripletsWithSmallerSum2(
+        [-1, 4, 2, 1, 3],
+        5
+      )
+
+      expect(result).toEqual([
+        [-1, 1, 4],
+        [-1, 1, 3],
+        [-1, 1, 2],
+        [-1, 2, 3]
+      ])
     })
   })
 })
